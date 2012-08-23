9bd278b166743811d584cceba3cc5a810c04501c
John F. Brown <jack@brownjohnf.com>
2012-08-23 20:41:52 +0000

    stopped app initialization during asset precompile

--- config/application.rb
+++ config/application.rb
@@ -61,6 +61,8 @@ module MounceEndo
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    # ... but don't attempt to initialize while compiling, or Heroku will fail
+    config.assets.initialize_on_precompile = false
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
