7df7154bd3693cc7652d6ab4c903518d96e7825a
John F. Brown <jack@brownjohnf.com>
2012-08-23 20:17:14 +0000

    initialized app; db:create; db:migrate; added newrelic config

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MounceEndo::Application.config.secret_token = '741ecd3b99d79b8100f10eff1819c8158f8f31e3afb7056658dfaef0d427c3c2f028db2075c66f218142c629d9b90f3d7f0a5ffb156893214f3c9280df538089'
